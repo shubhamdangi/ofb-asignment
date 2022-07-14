a64a24e6bda3e8471bc8b7eff8dcefb6bb6067ac
shubhamdangi <shubham.d3v@gmail.com>
2022-07-14 14:24:26 +0530

    feat: fetch data

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="Header">
+      <div className="title">facebook/react</div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Issues.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+
+function Issues() {
+  const [data, setData] = useState([]);
+
+  const fetchData = () => {
+    fetch("https://api.github.com/repos/facebook/react/issues")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => setData(data));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="Issues">
+      <>
+        <div className="feedpage">
+          <div className="dash">
+            <div className="Head">
+              <br />
+            </div>
+            <div className="contents">
+              <ul class="list-group list-group-flush issue ">
+                <li class="list-group-item dash">
+                  <span style={{ color: "green" }}>&#9673; </span> 635 Open{" "}
+                  <span style={{ color: "grey" }}>&#10003; 10,104 Closed </span>
+                </li>
+                {data.map((user) => (
+                  <li class="list-group-item dash" key={user.id}>
+                    <h5>
+                      {" "}
+                      <span style={{ color: "green" }}>&#9673; </span>{" "}
+                      {user.title}{" "}
+                      <span
+                        style={{
+                          backgroundColor: "violet",
+                          borderRadius: "5px",
+                          fontSize: "15px",
+                        }}
+                      >
+                        Type:{user.user.type}
+                      </span>
+                    </h5>
+                    <p style={{ color: "grey" }}>
+                      #{user.number} by {user.user.login}
+                    </p>
+                    {user.labels.map((i) => {
+                      <p>{i.name}</p>;
+                    })}{" "}
+                  </li>
+                ))}
+
+                <li class="list-group-item dash"></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </>
+    </div>
+  );
+}
+
+export default Issues;
