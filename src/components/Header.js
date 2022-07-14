47dddf767e30bb1ad864e45dc2386743097ece62
shubhamdangi <shubham.d3v@gmail.com>
2022-07-14 15:00:01 +0530

    fix: improve header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,31 @@ import React from "react";
 function Header() {
   return (
     <div className="Header">
-      <div className="title">facebook/react</div>
+      <div className="title">
+        facebook/<b>react</b>
+        <span
+          style={{
+            color: "grey",
+            border: "1px solid black",
+            borderRadius: "5px",
+            marginLeft: "3px",
+            padding: "2px",
+          }}
+        >
+          Public
+        </span>
+        <br />
+        <div className="lower-header">
+          <p> &lt;&gt; Code</p>
+          <p style={{ borderBottom: "1px solid orange" }}> &#9673; Issues</p>
+          <p> &#8593; Pull requests</p>
+          <p> &#x23F5; Actions</p>
+          <p> &#9830; Projects</p>
+          <p> &#128214; Wiki </p>
+          <p>&#128737; Security</p>
+          <p>&#128202; Insights</p>
+        </div>
+      </div>
     </div>
   );
 }
